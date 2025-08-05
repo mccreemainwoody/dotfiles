bfd0a72c2aec40034699d827dca8fe6b57e1023c
Clément METZ <metz.clement@gmail.com>
2025-08-05 13:26:01 +0200

    [STYLE] added stylua.toml formatting file. formatted all files using the now-defined rules

--- dotfiles/nvim/.config/nvim/lua/config/core/init.lua
+++ dotfiles/nvim/.config/nvim/lua/config/core/init.lua
@@ -3,12 +3,12 @@ local keymap = require("config.core.keymap")
 local autocommands = require("config.core.autocommands")
 
 return {
-	setup_all = function()
-		vim_options.setup_vim_options()
-		keymap.setup_keymap()
-		autocommands.setup_autocommands()
-	end,
-	setup_vim_options = vim_options.setup_vim_options,
-	setup_keymap = keymap.setup_keymap,
-	setup_autocommands = autocommands.setup_autocommands,
+    setup_all = function()
+        vim_options.setup_vim_options()
+        keymap.setup_keymap()
+        autocommands.setup_autocommands()
+    end,
+    setup_vim_options = vim_options.setup_vim_options,
+    setup_keymap = keymap.setup_keymap,
+    setup_autocommands = autocommands.setup_autocommands,
 }
