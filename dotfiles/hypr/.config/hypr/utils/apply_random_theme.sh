712d3d531cb44243b9936f3fbc4bb8c2608fc369
Clément METZ <metz.clement@gmail.com>
2025-10-24 02:07:15 +0800

    [FEAT] implemented hypryaml-based theme switcher
    added hyprpaper.conf
    
    added themes/wallpapers/hyprpaper-bg folder

---
+++ dotfiles/hypr/.config/hypr/utils/apply_random_theme.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+set -e
+
+exit_with_error() {
+    echo "$1"
+    exit 1
+}
+
+CONFIG_DIR="$HOME/.config/hypr/hypryaml"
+
+command_exist=$(command -v hypryaml; echo $?)
+
+if [ ! command_exist ]; then
+    exit_with_error "can't run script: hypryaml is not found"
+fi
+
+chosen_config=$(ls "$CONFIG_DIR" | sort -R | head -n 1)
+
+hypryaml apply "$CONFIG_DIR/$chosen_config"
