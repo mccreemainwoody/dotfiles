9188b30365600f03e8ed974e5bd966c9029d8f03
Clément METZ <metz.clement@gmail.com>
2026-02-12 15:54:08 +0100

    feat: added flake.nix, home.nix, and flake.lock

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+    description = "Nix Home-Manager configuration for dotfiles";
+
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, home-manager, ... } @_ : {
+        homeConfigurations.default = import ./home.nix;
+    };
+}
