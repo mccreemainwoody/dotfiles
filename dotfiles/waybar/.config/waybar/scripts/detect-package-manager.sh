681a1a231ab4d275a8b000a27df3df96bc939a27
Clément METZ <metz.clement@gmail.com>
2026-02-20 14:39:40 +0100

    fix(waybar): updated package manager check order. nix is now checked last

--- dotfiles/waybar/.config/waybar/scripts/detect-package-manager.sh
+++ dotfiles/waybar/.config/waybar/scripts/detect-package-manager.sh
@@ -4,18 +4,18 @@ set -e
 
 export PACKAGE_MANAGER="undefined"
 
-if command -v emerge > /dev/null 2>&1; then
+if command -v nixos-rebuild > /dev/null 2>&1; then
+    export PACKAGE_MANAGER="nixos"
+elif command -v emerge > /dev/null 2>&1; then
     export PACKAGE_MANAGER="emerge"
 elif command -v pacman > /dev/null 2>&1; then
     export PACKAGE_MANAGER="pacman"
-elif command -v nixos-rebuild > /dev/null 2>&1; then
-    export PACKAGE_MANAGER="nixos"
-elif command -v home-manager > /dev/null 2>&1; then
-    export PACKAGE_MANAGER="home-manager"
-elif command -v nix > /dev/null 2>&1; then
-    export PACKAGE_MANAGER="nix"
 elif command -v dnf > /dev/null 2>&1; then
     export PACKAGE_MANAGER="dnf"
 elif command -v apt > /dev/null 2>&1; then
     export PACKAGE_MANAGER="apt"
+elif command -v home-manager > /dev/null 2>&1; then
+    export PACKAGE_MANAGER="home-manager"
+elif command -v nix > /dev/null 2>&1; then
+    export PACKAGE_MANAGER="nix"
 fi
