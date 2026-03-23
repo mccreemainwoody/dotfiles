b0bb822b43d1a3e2b6bf26043c8d8d903510ced1
Clément METZ <metz.clement@gmail.com>
2026-03-23 21:27:30 +0100

    fix(nvim): switch vimtex view method to general

--- dotfiles/nvim/.config/nvim/lua/plugins/vimtex.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -4,7 +4,7 @@ return {
         lazy = false, -- we don't want to lazy load VimTeX
         init = function()
             -- VimTeX configuration goes here, e.g.
-            vim.g.vimtex_view_method = "texpresso"
+            vim.g.vimtex_view_method = "general"
         end,
     },
 }
